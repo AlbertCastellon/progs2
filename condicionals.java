8d05bb9de94e48a8886159fec5622b85164a5741
Pepe Sufragio <albertkstyon@gmail.com>
2025-09-25 17:55:36 +0200

    condicionals

---
+++ condicionals.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class condicionals {
+    public static void main(String[] args) {
+        int nota;
+        Scanner escaner = new Scanner(System.in);
+        System.out.println("Introdueix la teva nota");
+        nota = escaner.nextInt();
+        if (nota >= 5) {
+            System.out.println("Estàs aprovat");
+        }else {
+            System.out.println("Estàs suspès");
+        }
+
+        //-----------------------
+
+        int nota2;
+        System.out.println("Introdueix la teva nota");
+        nota2 = escaner.nextInt();
+        if (nota2 < 5) {
+            System.out.println("Estàs suspès");
+        }else if (nota < 10) {
+            System.out.println("Estàs aprovat");
+        }else {
+            System.out.println("Perfecte!");
+        }
+
+        //----------------------
+
+        int nota3;
+        System.out.println("Introdueix la teva nota");
+        nota3 = escaner.nextInt();
+        if (nota < 0 || nota > 10) {
+            System.out.println("la nota introduida és invàlida");
+        } else if (nota < 5) {
+            System.out.println("Estàs suspès");          
+        }else {
+            System.out.println("Estàs aprovat");
+        }
+        escaner.close();
+    }
+    
+}
---
+++ exerciciCondicional.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class exerciciCondicional {
+    public static void main(String[] args) {
+        Scanner escaner = new Scanner(System.in);
+
+            int edat;
+            boolean soci;
+
+            System.out.println("Quina edat tens?");
+            edat = escaner.nextInt();
+
+            if(edat < 18) {
+
+                System.out.println("No pots accedir a la discoteca per edat");
+
+            }else {
+
+                System.out.println("Ets soci?");
+                soci = escaner.nextBoolean();
+
+                if (soci) {
+
+                    System.out.println("Pots accedir gratuitament a la discoteca perquè tens el carnet");
+
+                }else if (edat >= 35) {
+
+                    System.out.println("Pots accedir gratuitament a la discoteca perquè ets més gran de 35");
+
+                }else {
+
+                    System.out.println("Pots accedir a la discoteca i cal que paguis 20 euros");
+                    
+                }
+            }
+           
+
+
+        escaner.close();
+    }
+}
